bcaf242244f81ec7f322ae754e19539d2c18919f
gaevskiyar <ag120461@ya.ru>
2019-01-28 19:41:22 +0500

    Initial commit

---
+++ app/src/main/java/com/example/gand/recyclerviewapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.gand.recyclerviewapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, RecyclerFragment.newInstance())
+                    .commit();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/gand/recyclerviewapplication/RecyclerFragment.java
@@ -0,0 +1,31 @@
+package com.example.gand.recyclerviewapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RecyclerFragment extends Fragment {
+
+    private RecyclerView mRecycler;
+
+    public static RecyclerFragment newInstance() {
+        
+       return  new RecyclerFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fr_recycler, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        mRecycler = view.findViewById(R.id.recycler);
+    }
+}
